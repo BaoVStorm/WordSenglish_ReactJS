03519be6283c2c7e58dcda278312d3852bde8808
VStorm <tranvubao2004@gmail.com>
2025-08-09 14:12:07 +0700

    add title

--- src/pages/Vocabulary/index.tsx
+++ src/pages/Vocabulary/index.tsx
@@ -6,7 +6,25 @@ import style from './Vocabulary.module.scss';
 const cx = classNames.bind(style);
 
 function Vocabulary(): JSX.Element {
-    return <>Vocabulary Pages</>;
+    return (
+        <>
+            <div className={cx("maxContainer")}>
+                <div className={cx("hero-blog-heading-content")}>
+                    <h2>Vocabularies:</h2>
+                </div>
+
+                <div className={cx("hero-blog-heading-content-sub")}>
+                    <p>
+                        Browse all vocabulary posts, explore new words with their meanings and examples, and easily review what you've learned. Expand your vocabulary step by step.
+                    </p>
+                </div>
+            </div>
+
+            <div className={cx("word-container")}>
+
+            </div>
+        </>
+    );
 }
 
 export default Vocabulary;
